2dd644680d16e46efb07e4ea1a5c43427d239cf9
Gabriel R F <github@gabrf.com>
2015-05-13 10:37:55 -0300

    Init

---
+++ Shell-Zabbix-WebScenario.sh
@@ -0,0 +1,85 @@
+HOSTNAME=“hostname”
+USER=“user”
+PASS=“password”
+HOMEPAGE=$1
+ESTADO=$2
+MUNICIPIO=$3
+
+if [ -z "$HOSTNAME" ]; 
+	then echo "Usage: addhost.sh <hostname> <ip>"
+	exit
+fi
+
+if [ -z "$IP" ];
+	then IP=0.0.0.0
+fi
+
+echo IP - $IP
+
+API=http://zabbix.interlegis.leg.br/zabbix/api_jsonrpc.php
+
+jsonval() {
+prop='sessionid'
+json=`curl --noproxy zabbix.interlegis.leg.br -i -X POST -H 'Content-Type: application/json-rpc' -d "
+{
+	\"jsonrpc\": \"2.0\",
+	\"method\": \"user.login\",
+	\"params\": {
+		\"user\": \"$USER\",
+		\"password\": \"$PASS\",
+		\"userData\":true
+	}, 
+	\"id\": 2
+}" $API`
+
+ temp=`echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop`
+echo ${temp##*|}
+}
+
+AUTH_TOKEN=$(jsonval)
+
+echo $AUTH_TOKEN
+echo '---------------------------------------------------'
+echo $MUNICIPIO $ESTADO $HOMEPAGE
+
+json2=`curl --noproxy zabbix.interlegis.leg.br -i -X POST -H 'Content-Type: application/json-rpc' -d "
+{
+	\"jsonrpc\":\"2.0\",
+	\"method\":\"httptest.create\",
+	\"params\":{
+		\"name\":\"$MUNICIPIO - $ESTADO\",
+		\"hostid\":\"10328\",
+		\"steps\":[{
+			\"name\":\"$HOMEPAGE\",
+			\"url\":\"$HOMEPAGE\",
+			\"status_codes\":200,
+			\"required\":\"Acesso\",
+			\"no\":1
+		}]
+	},
+	\"auth\":\"$AUTH_TOKEN\",
+	\"id\":1
+}" $API`
+
+temp3=`echo '{"jsonrpc":"2.0","result":{"httptestids":["598"]},"id":1}' | python -c 'import sys, json; obj=json.load(sys.stdin); print obj["result"]["httptestids"][0]'`
+echo "temp3:"
+echo ${temp3##*|}
+
+HTTPTESTIDS=$temp3
+
+json3=`curl --noproxy zabbix.interlegis.leg.br -i -X POST -H 'Content-Type: application/json-rpc' -d "
+{
+	\"jsonrpc\":\"2.0\",
+	\"method\":\"trigger.create\",
+	\"params\":{
+		\"description\":\"$MUNICIPIO - $ESTADO\",
+		\"expression\":\"{PortalModelo:web.test.fail[$MUNICIPIO - $ESTADO].last(0)}=0\",
+		\"dependencies\": []
+	},
+	\"auth\":\"$AUTH_TOKEN\",
+	\"id\": 1
+}" $API`
+
+temp4=`echo $json3`
+echo '---------------------------------------------------'
+echo ${temp4##*|}
